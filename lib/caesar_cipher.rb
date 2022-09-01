a86e4148876a6f925c954d58bfaa90c295b91960
sam-laptop <cwm9889@gmail.com>
2022-09-01 15:10:50 -0400

    Git is doing weird things

--- lib/caesar_cipher.rb
+++ lib/caesar_cipher.rb
@@ -5,15 +5,6 @@
 # Use TDD to create this solution
 class CaesarCipher
   def caesar_cipher(string, shift)
-    # A 65
-    # Z 90
-
-    # a 97
-    # z 122
-    # this would represent letter, 1 - 26
-    # string.ord - 97 
-    # add our shift to that, then use mod operand
-
     array = string.split('')
     array.map do |char|
       if char.ord.between?(65, 90) || char.ord.between?(97, 122)
@@ -24,10 +15,7 @@ class CaesarCipher
       end
     end.join
   end
-
 end
 
-require 'pry-byebug'
-# binding.pry
 cc = CaesarCipher.new
 puts cc.caesar_cipher('z Z!', 1)
